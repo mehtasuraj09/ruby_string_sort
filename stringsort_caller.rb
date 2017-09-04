5e2a75b08829a3ee6ae2340f72b563323ab74ec8
Suraj <mehtasuraj09@gmail.com>
2017-09-04 16:48:31 +0530

    Update stringsort_caller.rb

--- stringsort_caller.rb
+++ stringsort_caller.rb
@@ -6,3 +6,6 @@ string_to_sort = gets.chomp
 
 mostCharFirstObj = CharStringSort. new
 puts mostCharFirstObj.mostCharFirst string_to_sort
+
+# following is the new optimized method's call
+puts mostCharFirstObj.mostCharFirst2 string_to_sort
